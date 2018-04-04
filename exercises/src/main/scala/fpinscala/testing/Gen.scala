98fc15621acdc9343631325858897bad7746bff7
hoshikon <hoshikong@gmail.com>
2018-04-04 18:56:47 +0100

    Gen.unit, choose, boolean, listOfN

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -32,14 +32,31 @@ object Prop {
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
+case class Gen[A](sample: State[RNG,A])
+
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] = Gen(State(RNG.unit(a)))
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.nonNegativeLessThan(stopExclusive - start)).map(_ + start))
+
+  def boolean: Gen[Boolean] = Gen(State(RNG.int).map(_ >= 0))
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = {
+    val state = State((rng: RNG) => {
+      (0 to n).foldLeft((List.empty[A], rng))((acc, _) => {
+        val (list, r) = acc
+        val (x, r2) = g.sample.run(r)
+        (x :: list, r2)
+      })
+    })
+    Gen(state)
+    //Gen(State.sequence(List.fill(n)(g.sample))) //from answer
+  }
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
-}
+//trait Gen[A] {
+//  def map[A,B](f: A => B): Gen[B] = ???
+//  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+//}
 
 trait SGen[+A] {
 
