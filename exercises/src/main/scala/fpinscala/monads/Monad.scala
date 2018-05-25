9af01ec2fd8a1e79a678b5cf5addfb7bbc2d6611
hoshikon <hoshikong@gmail.com>
2018-05-25 10:51:25 +0100

    monads - sequence, traverse

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -36,9 +36,9 @@ trait Monad[M[_]] extends Functor[M] {
   def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
     flatMap(ma)(a => map(mb)(b => f(a, b)))
 
-  def sequence[A](lma: List[M[A]]): M[List[A]] = ???
+  def sequence[A](lma: List[M[A]]): M[List[A]] = lma.foldLeft(unit(List.empty[A]))(map2(_, _)(_ :+ _))
 
-  def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] = ???
+  def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] = sequence(la.map(f))
 
   def replicateM[A](n: Int, ma: M[A]): M[List[A]] = ???
 
@@ -95,7 +95,7 @@ object Monad {
     }
   }
 
-  val idMonad: Monad[Id] = ???
+  val idMonad: Monad[Id] = null//???
 
   def readerMonad[R] = ???
 }
--- exercises/src/test/scala/fpinscala/monads/MonadTest.scala
+++ exercises/src/test/scala/fpinscala/monads/MonadTest.scala
@@ -4,7 +4,11 @@ import fpinscala.SimpleBooleanTest
 
 object MonadTest extends App with SimpleBooleanTest {
   override def run: Unit = {
+    val sequenceTest = Monad.optionMonad.sequence(List(Some(1), Some(2), Some(3))) == Some(List(1,2,3))
+    printTest(sequenceTest, "sequence")
 
+    val traverseTest = Monad.optionMonad.traverse(List(1,2,3))(n => Some(n)) == Some(List(1,2,3))
+    printTest(traverseTest, "traverse")
   }
 
   run
