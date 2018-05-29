fe95fbcf5273f21eb8492f8595fcd37b06a71424
hoshikon <hoshikong@gmail.com>
2018-05-29 17:16:56 +0100

    applicatives - apply, map, map2

--- exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -13,7 +13,11 @@ trait Applicative[F[_]] extends Functor[F] {
 
   def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
 
-  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = ???
+  def map2WithApply[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply(map(fa)(f.curried))(fb)
+//    apply(map[A, B => C](fa)(a => b => f(a, b)))(fb)
+
+  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = map2(fab, fa)(_(_))
 
   def unit[A](a: => A): F[A]
 
